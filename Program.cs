f25e18ce9b7966e55c7f1ed174ec1956c0020b45
Panna Kristof <panna.kristof@prefixbox.com>
2020-03-27 14:58:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+
+            bool goodDog = true;
+            Console.WriteLine("It's {0}, my favorite number is {1}. And yours? ", goodDog, 42.0);
+
+            int yourFavorite;
+            int.TryParse(Console.ReadLine(), out yourFavorite);
+            Console.WriteLine("Your favorite is {0}.", yourFavorite);
+
+            int alma = 1;
+            alma += 3 + 4 + 5;
+            alma++;
+            Console.WriteLine(alma);
+
+        }
+
+        private static void SayHello()
+        {
+            Console.WriteLine("Hello PrefixBox!");
+        }
+    }
+}
